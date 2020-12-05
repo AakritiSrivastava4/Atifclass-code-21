f4a8085cd293fe7f77f0db4286d1ea33f059214e
AakritiSrivastava4 <71749750+AakritiSrivastava4@users.noreply.github.com>
2020-12-05 18:00:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,36 +1,26 @@
 
-const Bodies = Matter.Bodies
-const World= Matter.World   
-const Engine= Matter.Engine;
+const Bodies = Matter.Bodies ;
+const World= Matter.World ;
+const Engine= Matter.Engine; 
+var engine, world; 
+var ball ;
+ function setup(){ 
+   var canvas=createCanvas(400,400); 
+   engine = Engine.create (); 
+   world= engine.world; 
+   ball = Bodies.circle (200,200,10,{restitution:0.7}); 
+   World.add (world,ball) 
+   ground = Bodies.rectangle (200,390,400,10,{isStatic :true}); 
+   World.add (world,ground) 
+  } 
+   function draw() {
+      Engine.run(engine)
+       background(0); 
+       rectMode(CENTER) 
+       var pos = ball.position; 
 
-var engine, world;
-var box ;
-var ground;
-function setup(){
-  var canvas=createCanvas(400,400);
- engine = Engine.create ();
- world= engine.world;
-box = Bodies.rectangle (200,200,100,100);
-World.add (world,box)
+       circle (pos.x,pos.y,10) 
+       var pos1 = ground.position;
+        rect(pos1.x,pos1.y,400,10); }
 
- var ground_options ={
-        isStatic: true
-    }
 
-    ground = Bodies.rectangle(200,390,200,20,ground_options);
-    World.add(world,ground);
-
-}
-function draw()
-{
-  Engine.run(engine)
-  background(0);
-   rectMode(CENTER)
-  var pos = box.position;
-  console.log (pos)
-  rect (pos.x,pos.y,100,100)
-  
-      rectMode(CENTER);
-    rect(ground.position.x,ground.position.y,400,20);
-    
-}
